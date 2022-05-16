080fd74f887153368644b27c068cf407cd03959a
jctaylor <jctaylor210@gmail.com>
2022-05-16 15:28:40 -0500

    Started new App

---
+++ components/PrimaryButton.js
@@ -0,0 +1,12 @@
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default PrimaryButton;
